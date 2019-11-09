928696eb6ad28d50a2a229732c0763e03afd3f60
Dabug123 <clarkbrunswick55@gmail.com>
2019-11-09 04:46:42 +0100

    q3.1

--- franken.sh
+++ franken.sh
@@ -21,7 +21,7 @@ export LD_LIBRARY_PATH=${HOME}/android/clangop/clang-r365631c/lib64:$LD_LIBRARY_
 DEFCONFIG="floral_defconfig"
 
 # Kernel Details
-VER=".QU2"
+VER=".QU3.1"
 
 # Paths
 KERNEL_DIR=`pwd`
